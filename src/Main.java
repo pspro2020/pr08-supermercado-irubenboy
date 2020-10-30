732bd0b80a2317db937d59977a8e99f608f2b43a
Rubeb <rubenmoreno.saladillo@gmail.com>
2020-10-30 14:52:21 +0100

    Realización del main
    Se ha realizado el main del proyecto y se ha ejecutado.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import classes.Client;
+import classes.Supermarket;
+
 public class Main {
+    private static final int MAX_CLIENTS = 50;
+
+    public static void main(String[] args) {
+        Supermarket ruizGalan = new Supermarket(4);
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            new Thread(new Client(ruizGalan), "Client-" + i);
+        }
+    }
 }
